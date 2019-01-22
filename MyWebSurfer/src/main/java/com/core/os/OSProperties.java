b957c743e6eb4ea112b899f53ccca568c3f05bbb
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-22 14:03:20 +0100

    new classes for reading properties from system.

---
+++ MyWebSurfer/src/main/java/com/core/os/OSProperties.java
@@ -0,0 +1,58 @@
+package com.core.os;
+/**
+ * 
+ * @author gentjan koliçaj
+ *
+ */
+public class OSProperties {
+	
+	private String name;
+	private String version;
+	private String architecture;
+	
+	public OSProperties() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OSProperties(String name, String version, String architecture) {
+		super();
+		this.name = name;
+		this.version = version;
+		this.architecture = architecture;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getArchitecture() {
+		return architecture;
+	}
+
+	public void setArchitecture(String architecture) {
+		this.architecture = architecture;
+	}
+
+	@Override
+	public String toString() {
+		return "OSProperties [name=" + name + ", version=" + version + ", architecture=" + architecture + "]";
+	}
+
+	
+	
+	
+
+}
