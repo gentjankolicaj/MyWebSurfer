0b98ece3fed52b5870c5ebe483306698cfb28d4e
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-23 17:27:16 +0100

    new classes at surfer package.

---
+++ MyWebSurfer/src/main/java/com/core/surfer/SurferInfo.java
@@ -0,0 +1,13 @@
+package com.core.surfer;
+
+import java.util.List;
+/**
+ * 
+ * @author gentjan koliçaj
+ *
+ */
+public interface SurferInfo {
+	
+	public abstract List<Object> getSurferInfo() ;
+
+}
