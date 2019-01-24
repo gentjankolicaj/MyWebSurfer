7d783a4136f3d43aec6399719fb28583a9db5789
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-01-24 15:08:19 +0100

    new exception classes.

---
+++ MyWebSurfer/src/main/java/com/core/exception/WebBrowserException.java
@@ -0,0 +1,35 @@
+package com.core.exception;
+/**
+ * 
+ * @author gentjan koliçaj
+ *
+ */
+public class WebBrowserException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6570050850510298085L;
+
+	public WebBrowserException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebBrowserException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebBrowserException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebBrowserException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ MyWebSurfer/src/main/java/com/core/exception/WebSiteException.java
@@ -0,0 +1,38 @@
+package com.core.exception;
+
+/**
+ * 
+ * @author gentjan koliçaj
+ *
+ */
+public class WebSiteException extends RuntimeException{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6134356298424470339L;
+
+	public WebSiteException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebSiteException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebSiteException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WebSiteException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
